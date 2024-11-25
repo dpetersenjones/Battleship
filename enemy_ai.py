919a7c5dbc9874b39d2b8a418ea5514ac5aef0c8
dpetersen-jones <duncan.petersenjones@gmail.com>
2024-11-25 09:48:55 -0600

    Create the functions that allow the response to work

--- enemy_ai.py
+++ enemy_ai.py
@@ -6,6 +6,9 @@ class EnemyAI:
         self.directions = [[0,1], [0,-1], [1, 0], [-1, 0]]
         self.last_hit = False
         self.last_dir = False
+        self.curr_hit_streak = []
+        self.dir_tried_on_attempt = []
+        self.dfs = False
 
     def shoot(self, pos=False):
         if self.last_hit != False:
@@ -22,14 +25,49 @@ class EnemyAI:
                 return [y,x]
     
     def response(self, pos, hit=False, boat=False):
-        if hit and boat:
-            self.last_hit =False
-            self.last_dir = False
-        elif hit and not boat and self.last_dir != False:
+        if hit == True:
             self.last_hit = pos
-            new_pos = [pos[0]+self.last_dir[0], pos[1]+self.last_dir[1]]
 
 
-        elif not hit and self.last_hit != False and self.last_dir != False:
-            self.last_dir += 1
-            new_pos = [self.last_hit[0]+self.directions[self.last_dir][0], self.last_hit[1]+self.directions[self.last_dir][0]]
+
+
+
+
+
+
+        #The first hit is positive
+        def first_hit():
+            self.last_dir = self.directions[randint(0,3)]
+            next_hit = [(pos[0] + self.directions[0]), (pos[1] + self.directions[1])]
+            if next_hit in self.attempted_coor:
+                return first_hit()
+            self.dir_tried_on_attempt.append(self.last_dir)
+            return next_hit
+        
+        #Continues to fire in the same direction
+        # if that hit is has already been attempted
+        # then it changes to the change_direction function
+        def continue_direction():
+            next_hit = [(pos[0] + self.directions[0]), (pos[1] + self.directions[1])]
+            if next_hit in self.attempted_coor:
+                return change_direction()
+            return next_hit
+        
+        def change_direction():
+            if len(self.curr_hit_streak) <= 1:
+                if [pos[0] + (0 - self.last_dir[0]), pos[1] + (0 - self.last_dir[1])] not in self.attempted_coor:
+                    return [pos[0] + (0 - self.last_dir[0]), pos[1] + (0 - self.last_dir[1])]
+                for d in self.directions:
+                    if [pos[0] + (0 - d[0]), pos[1] + (0 - d[1])] not in self.attempted_coor:
+                        return [pos[0] + (0 - self.last_dir[0]), pos[1] + (0 - self.last_dir[1])]
+            else:
+                self.last_dir[0] = 0 - self.last_dir[0]
+                self.last_dir[1] = 0 - self.last_dir[1]
+                next_hit = pos
+                while pos in self.curr_hit_streak and pos in self.attempted_coor:
+                    pos[0] = pos[0] + self.last_dir[0]
+                    pos[1] = pos[1] + self.last_dir[1]
+                if pos in self.attempted_coor:
+                    self.dfs = True
+                    return dfs()
+                return pos
