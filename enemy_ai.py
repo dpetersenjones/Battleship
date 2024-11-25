c173c8e2d385777b0dd5959c67c56a9b9b78b402
dpetersen-jones <duncan.petersenjones@gmail.com>
2024-11-25 10:04:44 -0600

    Adding logic to response function

--- enemy_ai.py
+++ enemy_ai.py
@@ -7,7 +7,6 @@ class EnemyAI:
         self.last_hit = False
         self.last_dir = False
         self.curr_hit_streak = []
-        self.dir_tried_on_attempt = []
         self.dfs = False
 
     def shoot(self, pos=False):
@@ -25,15 +24,26 @@ class EnemyAI:
                 return [y,x]
     
     def response(self, pos, hit=False, boat=False):
-        if hit == True:
+        if not hit and not self.last_hit:
+            return False
+        elif hit and boat:
+            self.last_hit = False
+            self.last_dir = False
+            self.dfs = False
+            self.curr_hit_streak = []
+            return False
+        elif hit and not self.last_hit:
             self.last_hit = pos
-
-
-
-
-
-
-
+            self.curr_hit_streak.append(pos)
+            next_hit = first_hit()
+        elif hit and self.last_hit:
+            next_hit = continue_direction()
+        elif self.dfs:
+            next_hit = dfs()
+        elif not hit and self.last_hit:
+            next_hit = change_direction()
+        else:
+            next_hit = dfs()
 
         #The first hit is positive
         def first_hit():
@@ -41,7 +51,6 @@ class EnemyAI:
             next_hit = [(pos[0] + self.directions[0]), (pos[1] + self.directions[1])]
             if next_hit in self.attempted_coor:
                 return first_hit()
-            self.dir_tried_on_attempt.append(self.last_dir)
             return next_hit
         
         #Continues to fire in the same direction
@@ -53,6 +62,9 @@ class EnemyAI:
                 return change_direction()
             return next_hit
         
+        #If it is on the first hit, it tries the other direction
+        #However, if it is the end of a line and the ship hasn't sucken, then it checks the other direction
+        #If the other direction doesn't come up with a sinking status, then it does a dfs
         def change_direction():
             if len(self.curr_hit_streak) <= 1:
                 if [pos[0] + (0 - self.last_dir[0]), pos[1] + (0 - self.last_dir[1])] not in self.attempted_coor:
@@ -71,3 +83,15 @@ class EnemyAI:
                     self.dfs = True
                     return dfs()
                 return pos
+            
+        def dfs():
+            for p in self.curr_hit_streak:
+                for d in self.directions:
+                    if [p[0] + d[0], p[1] + d[1]] not in self.attempted_coor:
+                        return [p[0] + d[0], p[1] + d[1]]
+                    
+
+        if next_hit in self.attempted_coor:
+            print("Something Fucked up")
+            return False
+        return next_hit
